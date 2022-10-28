43f3ef4f082e81ef4b701ffe0d988e31fd07e886
SANGOROISAAC <93325427+SANGOROISAAC@users.noreply.github.com>
2022-10-28 09:34:44 +0300

    Add files via upload

---
+++ framework.js
@@ -0,0 +1,13 @@
+function Myfunction(){
+    document.getElementById(demo).innerHTML="Welcome to Real Estate Market";
+
+
+}
+
+    alert("Real Estate Market is booming");
+
+    prompt("People are lucky there is a website Application for that");
+
+
+
+
